AlexandriaLittle/dof-helpers

commit 8c7212f8a9d4adecda44d99c7f21645ad3558c67

Create more helpful console.log outputs

parseComponent.js
@@ -69,7 +69,15 @@ function parseComponent(componentPath, outDir, compModelName) {
 	
 	try {
 		// read in sub-components
-		var componentsList = fs.readdirSync(componentPath + "/components/").sort();
+		
+		try {
+			var componentsList = fs.readdirSync(componentPath + "/components/").sort();	
+		} catch (error) {
+			console.log("Experienced error accessing folder: " + componentPath + "/components/")
+		}
+		
+		
+		
 		for (i in componentsList) {
 			var subComponent = parseComponent(componentPath + "/components/" + componentsList[i], outDir, compModelName);
 			component.components[subComponent.name] = subComponent;
@@ -92,7 +100,7 @@ function parseComponent(componentPath, outDir, compModelName) {
 									.parseAndRender(component.assemblySteps[s].summary, component)
 									.then(function(renderedSummary) {
 										component.assemblySteps[s].summary = renderedSummary;
-										console.log(component.assemblySteps[s].summary) //ANL
+										// console.log(component.assemblySteps[s].summary) //ANL
 									}).catch(function(e) {
 										console.log(e);
 									}));
@@ -154,11 +162,16 @@ function parseComponent(componentPath, outDir, compModelName) {
 	Promise.allSettled(liquidPromises)
 		.then(function() {
 			var componentFileName = componentPath + '/' + outDir + '/' + compModelName;
-			fs.writeFileSync(componentFileName, yaml.safeDump(component));
-			console.log('Component Model ' + componentFileName + ' built');
-				}).catch(function(e) {
-			console.log(e);
-		});
+			try {
+				
+				fs.writeFileSync(componentFileName, yaml.safeDump(component));	
+				console.log('Component Model ' + componentFileName + ' built');
+
+			} catch (error) {
+				console.log("Experienced error accessing file: " + componentFileName);
+			}
+			
+				});
 	
 	return component;
 }

commit badcfd8d6a2f5767dd349b8c73c8670c14bf305f

Attempt fix for bug introduced in commit 8c7212f8a9d4adecda44d99c7f21645ad3558c67

parseComponent.js
@@ -74,6 +74,7 @@ function parseComponent(componentPath, outDir, compModelName) {
 			var componentsList = fs.readdirSync(componentPath + "/components/").sort();	
 		} catch (error) {
 			console.log("Experienced error accessing folder: " + componentPath + "/components/")
+			throw new Error("Experienced error accessing folder: " + componentPath + "/components/")
 		}
 		
 		